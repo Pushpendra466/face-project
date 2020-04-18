2b2b17466832c6d73b76adcd6a5a8346cf963005
pushpendra kumar <pushpendra466@gmail.com>
2020-04-18 21:04:12 +0530

    Updates

---
+++ precache-manifest.89f781f35f45b9a391073e7ae01e279a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01586759b2a8fa2d9ecf08c5440bafd8",
+    "url": "/face-project/index.html"
+  },
+  {
+    "revision": "4653ec59ae6e65088251",
+    "url": "/face-project/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "6d4ae16f5bf348c3ad16",
+    "url": "/face-project/static/css/main.14514a0f.chunk.css"
+  },
+  {
+    "revision": "4653ec59ae6e65088251",
+    "url": "/face-project/static/js/2.3f3600ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-project/static/js/2.3f3600ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4ae16f5bf348c3ad16",
+    "url": "/face-project/static/js/main.1f53af5d.chunk.js"
+  },
+  {
+    "revision": "c098f2c0d62e22d7c389",
+    "url": "/face-project/static/js/runtime-main.c6d00e85.js"
+  }
+]);
